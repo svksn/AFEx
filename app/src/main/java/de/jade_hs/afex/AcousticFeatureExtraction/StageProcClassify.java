f6f755e902730369a0d94bb3518c8ae97fdecaed
Sven Kissner <sven.kissner@jade-hs.com>
2025-04-03 08:46:59 +0200

    - fix input length for yamnet - increase timeout accordingly - send classification results to writer stage - update dependencies

--- app/src/main/java/de/jade_hs/afex/AcousticFeatureExtraction/StageProcClassify.java
+++ app/src/main/java/de/jade_hs/afex/AcousticFeatureExtraction/StageProcClassify.java
@@ -6,6 +6,7 @@ import com.google.mediapipe.tasks.audio.core.RunningMode;
 import com.google.mediapipe.tasks.components.containers.AudioData;
 import com.google.mediapipe.tasks.core.BaseOptions;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -28,13 +29,13 @@ public class StageProcClassify extends Stage {
             AudioClassifier.AudioClassifierOptions.builder()
                     .setBaseOptions(baseOptions)
                     .setRunningMode(RunningMode.AUDIO_CLIPS)
-                    .setMaxResults(5)
+                    .setMaxResults(3)
                     .build();
         ;
 
         audioClassifier = AudioClassifier.createFromOptions(context, options);
 
-        // input length for yamnet is 0.96 * 16000 = 15360 samples = blockSize
+        // input length for yamnet is 0.975 * 16000 = 15600 samples = blockSize
         audioData = AudioData.create(AudioData.AudioDataFormat.builder()
                                 .setNumOfChannels(1)
                                 .setSampleRate(samplingrate)
@@ -46,15 +47,20 @@ public class StageProcClassify extends Stage {
     @Override
     protected void process(float[][] buffer) {
 
-        //float[][] dataOut = new float[buffer.length][1];
+        float[][] dataOut = new float[buffer.length][6]; // 3 results/channel, each with id and score interleaved
 
-        //for (int i = 0; i < buffer.length; i++) {
-            audioData.load(buffer[0]);
+        for (int channel = 0; channel < buffer.length; channel++) {
+            audioData.load(buffer[channel]);
             AudioClassifierResult result = audioClassifier.classify(audioData);
-            System.out.println("----------------> CLASSIFY: " + result);
-        //}
 
-        //send(dataOut);
+            for (int i = 0; i < 3; i++) {
+                dataOut[channel][i * 2] = result.classificationResults().get(0).classifications().get(0).categories().get(i).index();
+                dataOut[channel][i * 2 + 1] = result.classificationResults().get(0).classifications().get(0).categories().get(i).score();
+            }
+
+        }
+        System.out.println(Arrays.deepToString(dataOut));
+        send(dataOut);
     }
 
 
