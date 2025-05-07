82fee7c5b29786944f3cb82a5dd9c90fb7f35cda
Sven Kissner <sven.kissner@jade-hs.de>
2025-05-07 16:14:50 +0200

    add Silero VAD (untested)

---
+++ app/src/main/java/de/jade_hs/afex/AcousticFeatureExtraction/StageProcVAD.java
@@ -0,0 +1,56 @@
+package de.jade_hs.afex.AcousticFeatureExtraction;
+
+
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import com.konovalov.vad.silero.Vad;
+import com.konovalov.vad.silero.VadSilero;
+import com.konovalov.vad.silero.config.FrameSize;
+import com.konovalov.vad.silero.config.Mode;
+import com.konovalov.vad.silero.config.SampleRate;
+
+import java.util.HashMap;
+
+/**
+ * Feature: Voice Activity Detection using Silero
+ * https://github.com/gkonovalov/android-vad/
+ */
+
+public class StageProcVAD extends Stage {
+
+    final static String LOG = "StageProcVAD";
+    private final VadSilero vad;
+
+    public StageProcVAD(HashMap parameter) {
+        super(parameter);
+
+        vad = Vad.builder()
+                .setContext(context)
+                .setSampleRate(SampleRate.SAMPLE_RATE_16K)
+                .setFrameSize(FrameSize.FRAME_SIZE_512)
+                .setMode(Mode.NORMAL)
+                .setSilenceDurationMs(300)
+                .setSpeechDurationMs(50)
+                .build();
+    }
+
+
+    @Override
+    protected void process(float[][] buffer) {
+
+        //float[][] dataOut = new float[buffer.length][1];
+        //for (int i = 0; i < buffer.length; i++) {
+        boolean isSpeech = vad.isSpeech(buffer[0]);
+        System.out.println("----------------> VAD: " + isSpeech);
+        //}
+
+        //send(dataOut);
+    }
+
+    @Override
+    protected void cleanup() {
+        super.cleanup();
+        vad.close();
+    }
+
+}
